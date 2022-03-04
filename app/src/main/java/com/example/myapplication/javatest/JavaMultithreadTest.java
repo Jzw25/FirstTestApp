a84b5085d1e072d677adf18594a486518443ae9e
King <2250847606@qq.com>
2022-03-04 17:17:55 +0800

    Jzw33 20200304

--- app/src/main/java/com/example/myapplication/javatest/JavaMultithreadTest.java
+++ app/src/main/java/com/example/myapplication/javatest/JavaMultithreadTest.java
@@ -4,9 +4,12 @@ import android.os.SystemClock;
 import android.util.Log;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -893,6 +896,154 @@ public class JavaMultithreadTest {
              * ，在等固定的时间，然后执行。即：执行完上一个任务后再执行）。
              */
             scheduledExecutorService.scheduleAtFixedRate(new MyTestRunable(),0,5,TimeUnit.SECONDS);
+        }
+    }
+
+    /**
+     * 我们知道创建线程的方式有两种，一种是实现Runnable接口，另一种是继承Thread，但是这两种方式都有个缺点，那就
+     * 是在任务执行完成之后无法获取返回结果，那如果我们想要获取返回结果该如何实现呢？使用Callable和Future，通过
+     * 它们构建的线程，在任务执行完成后就可以获取执行结果
+     *
+     * Callable<V>接口：
+     * 定义：
+     * public interface Callable<V> {
+     *       V   call()   throws Exception;
+     * }
+     * 该接口声明了一个名称为call()的方法，同时这个方法可以有返回值V，也可以抛出异常。论是Runnable接口的实现类
+     * 还是Callable接口的实现类，都可以被ThreadPoolExecutor或ScheduledThreadPoolExecutor执行，
+     * ThreadPoolExecutor或ScheduledThreadPoolExecutor都实现了 ExcutorService接口， 而因此
+     * Callable需要和Executor框架中的ExcutorService结合使用，ExecutorService提供的方法：
+     * <T> Future<T> submit(Callable<T> task);
+     * <T> Future<T> submit(Runnable task, T result);
+     * Future<?> submit(Runnable task);
+     * 第一个方法：submit提交一个实现Callable接口的任务，并且返回封装了异步计算结果的Future。
+     * 第二个方法：submit提交一个实现Runnable接口的任务，并且指定了在调用Future的get方法时返回的result对象。
+     * 第三个方法：submit提交一个实现Runnable接口的任务，并且返回封装了异步计算结果的Future。
+     * 因此我们只要创建好我们的线程对象（实现Callable接口或者Runnable接口），然后通过上面3个方法提交给线程池去
+     * 执行即可。还有点要注意的是，除了我们自己实现Callable对象外，我们还可以使用工厂类Executors来把一个Runnable
+     * 对象包装成Callable对象。
+     * public static Callable<Object> callable(Runnable task)
+     * public static <T> Callable<T> callable(Runnable task, T result)
+     *
+     * Future<V>接口：
+     * Future<V>接口是用来获取异步计算结果的，说白了就是对具体的Runnable或者Callable对象任务执行的结果进行获取
+     * (get()),取消(cancel()),判断是否完成等操作
+     * public interface Future<V> {
+     *     boolean cancel(boolean mayInterruptIfRunning);
+     *     boolean isCancelled();
+     *     boolean isDone();
+     *     V get() throws InterruptedException, ExecutionException;
+     *     V get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException;
+     * }
+     * V get() ：获取异步执行的结果，如果没有结果可用，此方法会阻塞直到异步计算完成。
+     * V get(Long timeout , TimeUnit unit) ：获取异步执行结果，如果没有结果可用，此方法会阻塞，但是会有时间
+     * 限制，如果阻塞时间超过设定的timeout时间，该方法将抛出异常。
+     * boolean isDone() ：如果任务执行结束，无论是正常结束或是中途取消还是发生异常，都返回true。
+     * boolean isCanceller() ：如果任务完成前被取消，则返回true。
+     * boolean cancel(boolean mayInterruptRunning) ：如果任务还没开始，执行cancel(...)方法将返回false；
+     * 如果任务已经启动，执行cancel(true)方法将以中断执行此任务线程的方式来试图停止任务，如果停止成功，返回true；
+     * 当任务已经启动，执行cancel(false)方法将不会对正在执行的任务线程产生影响(让线程正常执行到完成)，此时返回
+     * false；当任务已经完成，执行cancel(...)方法将返回false。mayInterruptRunning参数表示是否中断执行中的线程。
+     * 通过方法分析我们也知道实际上Future提供了3种功能：（1）能够中断执行中的任务（2）判断任务是否执行完成（3）
+     * 获取任务执行完成后额结果。
+     *
+     * FutureTask类：
+     * public class FutureTask<V> implements RunnableFuture<V>
+     * FutureTask类实现了RunnableFuture接口，我们看一下RunnableFuture接口的实现：
+     * public interface RunnableFuture<V> extends Runnable, Future<V> {
+     *     void run();
+     * }
+     *
+     * FutureTask除了实现了Future接口外还实现了Runnable接口，因此FutureTask也可以直接提交给Executor执行。
+     * 当然也可以调用线程直接执行（FutureTask.run()）。接下来我们根据FutureTask.run()的执行时机来分析其所处的3种状态：
+     * （1）未启动，FutureTask.run()方法还没有被执行之前，FutureTask处于未启动状态，当创建一个FutureTask，
+     * 而且没有执行FutureTask.run()方法前，这个FutureTask也处于未启动状态。
+     * （2）已启动，FutureTask.run()被执行的过程中，FutureTask处于已启动状态。
+     * （3）已完成，FutureTask.run()方法执行完正常结束，或者被取消或者抛出异常而结束，FutureTask都处于完成状态。
+     *
+     * FutureTask方法执行示意图：hdpi下futuretask
+     * （1）当FutureTask处于未启动或已启动状态时，如果此时我们执行FutureTask.get()方法将导致调用线程阻塞；
+     * 当FutureTask处于已完成状态时，执行FutureTask.get()方法将导致调用线程立即返回结果或者抛出异常。
+     * （2）当FutureTask处于未启动状态时，执行FutureTask.cancel()方法将导致此任务永远不会执行。
+     * 当FutureTask处于已启动状态时，执行cancel(true)方法将以中断执行此任务线程的方式来试图停止任务，
+     * 如果任务取消成功，cancel(...)返回true；但如果执行cancel(false)方法将不会对正在执行的任务线程产生影响
+     * (让线程正常执行到完成)，此时cancel(...)返回false。
+     * 当任务已经完成，执行cancel(...)方法将返回false。
+     * 构造函数：
+     * public FutureTask(Callable<V> callable) {
+     * }
+     * public FutureTask(Runnable runnable, V result) {
+     * }
+     */
+    public void tryCallableTest(){
+        /**
+         * 假设有一个耗时计算操作要做
+         */
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        //执行线程
+        Future<Integer> future = executorService.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                Log.d(TAG, "call: this is thread do sum");
+                int sum = 0;
+                Thread.sleep(2000);
+                for (int i = 0; i < 500; i++) {
+                    sum++;
+                }
+                Log.d(TAG, "call: this is thread do sum finish!");
+                return sum;
+            }
+        });
+        //执行完后关闭线程池
+        executorService.shutdown();
+        try {
+            Thread.sleep(2000);
+            //主线程做其他事情
+        }catch (Exception e){
+
+        }
+        try {
+            if(future.get()!=null){
+                Log.d(TAG, "tryCallableTest: we get the callable result is : " + future.get());
+            }else {
+                Log.d(TAG, "tryCallableTest: we can not get the callabel result!");
+            }
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void tryFutureTaskTest(){
+        //此方法将runabe对象包装成callable
+        Callable<Object> callable = Executors.callable(new MyTestRunable());
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        FutureTask<Integer> futureTask = new FutureTask<Integer>(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                Log.d(TAG, "call: this is thread do sum");
+                int sum = 0;
+                Thread.sleep(2000);
+                for (int i = 0; i < 500; i++) {
+                    sum++;
+                }
+                Log.d(TAG, "call: this is thread do sum finish!");
+                return sum;
+            }
+        });
+       executorService.submit(futureTask);
+       executorService.shutdown();
+        try {
+            Thread.sleep(2000);
+            //主线程做其他事情
+            if(futureTask.get()!=null){
+                Log.d(TAG, "tryFutureTaskTest: futureTask get the result is : " + futureTask.get());
+            }else {
+                Log.d(TAG, "tryFutureTaskTest: futureTask can not get the result");
+            }
+        }catch (Exception e){
+
         }
     }
 }
